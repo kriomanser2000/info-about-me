546bed99bb6b6a0f98897fea00a2ab0cc97d1047
Kriomanser2000 <kuzmenkasana05@gmail.com>
2024-07-12 12:41:45 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import HelloReactCreateElement from './components/HelloReactCreateElement';
+const HelloReact = () => 
+{
+  return <h1>Hello, React</h1>;
+};
+const PersonalInfo = () => 
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Особиста інформація</h2>
+      <p>Ім'я: Кузьменко Олександр</p>
+      <p>Контактний телефон: +380123456789</p>
+      <p>Електронна адреса: sasa05@gmail.com</p>
     </div>
   );
-}
-
+};
+const CityInfo = () => 
+{
+  return (
+    <div>
+      <h2>Інформація про місто</h2>
+      <p>Назва міста: Кривий Ріг</p>
+      <p>Назва країни: Україна</p>
+      <p>Рік заснування: 1775 рік</p>
+    </div>
+  );
+};
+const App = () => 
+{
+  return (
+    <div>
+      <HelloReact />
+      <PersonalInfo />
+      <CityInfo />
+    </div>
+  );
+};
 export default App;
---
+++ src/components/HelloReactCreateElement.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const HelloReactCreateElement = () => 
+{
+  return React.createElement('h1', null, 'Hello, React');
+};
+export default HelloReactCreateElement;
